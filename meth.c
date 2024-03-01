4c64f2acd3914cd93fc101740237005601c39ad0
robo-monk <rrobomonk@gmail.com>
2024-03-01 16:10:06 +0100

    tokenization

--- meth.c
+++ meth.c
@@ -1,22 +1,101 @@
 #include "stdio.h"
+#include "stdlib.h"
+
 #define true 1
 #define false 0
-#define string char*
+#define string char *
 
-void read_file(string filename) {
+typedef enum
+{
+    ADD,
+    MIN,
+    DIV,
+    MUL
+} OpType;
+
+typedef enum
+{
+    NUMBER,
+    OP,
+    OPEN_PAR,
+    CLOSE_PAR
+} TokenType;
+
+typedef union
+{
+    int n;
+    OpType op_type;
+} TokenValue;
+
+typedef struct
+{
+    TokenType type;
+    TokenValue value;
+} Token;
+
+Token new_op_token(TokenType type, OpType op_type)
+{
+    Token t = {
+        .type = type,
+        .value = {
+            .op_type = op_type}};
+    return t;
+}
+
+Token new_token(TokenType type)
+{
+    Token t = {
+        .type = type};
+    return t;
+}
+
+void tokenise(string filename, Token *tokens)
+{
     FILE *fptr;
     fptr = fopen(filename, "r");
 
+    int i = 0;
     char c;
-    while (c != -1) {
+    // Token *tokens = malloc(512 * sizeof(Token));
+
+    while (c != -1)
+    {
         c = fgetc(fptr);
         printf("%c", c);
-    }
 
+        switch (c)
+        {
+        case '+':
+            tokens[i] = new_op_token(OP, ADD);
+            break;
+        case '-':
+            tokens[i] = new_op_token(OP, MIN);
+            break;
+        case '/':
+            tokens[i] = new_op_token(OP, DIV);
+            break;
+        case '*':
+            tokens[i] = new_op_token(OP, MUL);
+            break;
+        case '(':
+            tokens[i] = new_token(OPEN_PAR);
+            break;
+        case ')':
+            tokens[i] = new_token(CLOSE_PAR);
+            break;
+        default:
+            break;
+        }
+
+        i++;
+    }
 }
 
-int main() {
+int main()
+{
     printf("hello \n");
-    read_file("./expr.meth");
+
+    Token *tokens = malloc(512 * sizeof(Token));
+    tokenise("./expr.meth", tokens);
     return 0;
 }
