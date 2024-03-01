713527e9ceaaf6b00edd69e78a07d173fae16c82
robo-monk <rrobomonk@gmail.com>
2024-03-01 16:35:52 +0100

    debug tokens

--- meth.c
+++ meth.c
@@ -1,24 +1,26 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define bool int
 #define true 1
 #define false 0
 #define string char *
 
 typedef enum
 {
-    ADD,
-    MIN,
-    DIV,
-    MUL
+    ADD='+',
+    MIN='-',
+    DIV='/',
+    MUL='*'
 } OpType;
 
 typedef enum
 {
     NUMBER,
     OP,
-    OPEN_PAR,
-    CLOSE_PAR
+    OPEN_PAR='(',
+    CLOSE_PAR=')',
+    PROGRAM_END
 } TokenType;
 
 typedef union
@@ -49,19 +51,51 @@ Token new_token(TokenType type)
     return t;
 }
 
+int parse_number(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    return -1;
+}
+
 void tokenise(string filename, Token *tokens)
 {
     FILE *fptr;
     fptr = fopen(filename, "r");
 
-    int i = 0;
+    int i = -1;
     char c;
+    Token current_token;
     // Token *tokens = malloc(512 * sizeof(Token));
+    bool is_constructing_multichar_token = false;
 
     while (c != -1)
     {
+        i++;
+
         c = fgetc(fptr);
-        printf("%c", c);
+        int number = parse_number(c);
+
+        if (number > -1) {
+            if (!is_constructing_multichar_token) {
+                is_constructing_multichar_token = true;
+                current_token = new_token(NUMBER);
+                current_token.value.n = number;
+            } else {
+                int current_token_value = current_token.value.n;
+                current_token_value <<= 1;
+                current_token_value += number;
+                current_token.value.n = current_token_value;
+            }
+            continue;
+        }
+
+        // done with construction
+        if (is_constructing_multichar_token) {
+            tokens[i] = current_token;
+            i++;
+            is_constructing_multichar_token = false;
+        }
 
         switch (c)
         {
@@ -84,18 +118,40 @@ void tokenise(string filename, Token *tokens)
             tokens[i] = new_token(CLOSE_PAR);
             break;
         default:
+            if (number > -1) {
+                // printf("%d ", number);
+            }
             break;
         }
+    }
 
+    if (is_constructing_multichar_token) {
+        tokens[i] = current_token;
+        i++;
+        is_constructing_multichar_token = false;
+    }
+    tokens[i] = new_token(PROGRAM_END);
+}
+
+void dbg_tokens(Token* tokens) {
+    int i = 0;
+    while (tokens[i].type != PROGRAM_END) {
+        Token current_token = tokens[i];
+        if (current_token.type == OP) {
+            printf("Operand: %c \n", current_token.value.op_type);
+        } else if (current_token.type == NUMBER) {
+            printf("Number: %d \n", current_token.value.n);
+        } else {
+            printf("Token: %c \n", current_token.type);
+        }
         i++;
     }
 }
 
 int main()
 {
-    printf("hello \n");
-
     Token *tokens = malloc(512 * sizeof(Token));
     tokenise("./expr.meth", tokens);
+    dbg_tokens(tokens);
     return 0;
 }
