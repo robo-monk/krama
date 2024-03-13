fba7e7c29f1cfdeefb4357128e81473ce002614e
robo-monk <rrobomonk@gmail.com>
2024-03-13 11:43:04 +0100

    add timing and shit

--- src/main.c
+++ src/main.c
@@ -4,18 +4,37 @@
 #include "frontend/Tokeniser.h"
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // Assuming these functions are defined elsewhere in your program
-void run_file(const char *filename) {
+void run_file(const string filename) {
+  // Start timing
+  clock_t start_time = clock();
+
+  // ANSI escape codes for colors and styles
+  const char *color_green = "\033[32m";
+  const char *color_yellow = "\033[33m";
+  const char *color_reset = "\033[0m";
+  const char *style_bold = "\033[1m";
+
+  printf("\n%s%s > krama run %s%s\n", style_bold, color_green, filename,
+         color_reset);
+
   Token *tokens = malloc(512 * sizeof(Token));
   tokenise_file(filename, tokens);
   BlockStatement *program = parse_program(tokens);
   ReturnValue ret_value = exec_program(program);
 
-  printf("---\n\n");
-  printf("%d", ret_value.i32_value);
+  // End timing
+  clock_t end_time = clock();
+  double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC *
+                      1000; // Convert to milliseconds
+
+  // Display result and execution time
+  printf("%s---%s\n\n", color_yellow, color_reset);
+  printf("%d\n", ret_value.i32_value);
+  printf("%s > Done in %.4fms%s\n", color_green, time_spent, color_reset);
 }
-// void run_inline_string(const char *str);
 
 int main(int argc, char *argv[]) {
   // Check if the only argument is "--help" or no arguments are passed
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 def fac(a):
-    if a == 0: return 0;
-    if a == 1: return 1;
-    return fac(a-1)*fac(a-2)
+    if a < 2: return a;
+    return fac(a-1) + fac(a-2)
 
-print(fac(33))
+print(fac(35))
